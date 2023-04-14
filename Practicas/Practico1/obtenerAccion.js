5de76cc5d1ab107b12d11464fbde7f0150516f68
Fernando Torrez <Fer.torrez@outlook.es>
2023-04-14 08:29:46 -0400

    Primer Parcial: parte practica

--- Practicas/Practico1/obtenerAccion.js
+++ Practicas/Practico1/obtenerAccion.js
@@ -22,7 +22,6 @@
   Si es externo y estado registro vigente: matricular
 
 */
-const assert = require("assert");
 
 function obtenerAccion(
   esObligatorio,
@@ -47,33 +46,3 @@ function obtenerAccion(
     return "matricular";
   }
 }
-
-// pruebas de junit test Para obtenerAccion
-
-// const obtenerAccion = require("./obteneraccion");
-
-describe("obtenerAccion", function () {
-  // Si es obligatorio y es docente: actualizar
-  it("Deberia devolver actualizar si es obligatorio y es docente", function () {
-    assert.equal(obtenerAccion("si", true, false, "", ""), "actualizar");
-  });
-  // Si no es obligatorio y es docente: matricular
-  it("Deberia devolver matricular si no es obligatorio y es docente", function () {
-    assert.equal(obtenerAccion("no", true, false, "", ""), "matricular");
-  });
-  // Si es externo y estado registro por confirmar: actualizar
-  it("Deberia devolver actualizar si es externo y estado registro por confirmar", function () {
-    assert.equal(
-      obtenerAccion("", true, true, "", "porConfirmar"),
-      "actualizar"
-    );
-  });
-  // Si es externo y destinatario externo: registrar
-  it("Deberia devolver registrar si es externo y destinatario externo", function () {
-    assert.equal(obtenerAccion("", true, true, "Externo", ""), "registrar");
-  });
-  // Si es externo y estado registro vigente: matricular
-  it("Deberia devolver matricular si es externo y estado registro vigente", function () {
-    assert.equal(obtenerAccion("", true, true, "", "Vigente"), "matricular");
-  });
-});
